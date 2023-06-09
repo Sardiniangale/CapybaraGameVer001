13ddce0db3936bf78c43e84d210db2b1811eb510
Sardiniangale <128549272+Sardiniangale@users.noreply.github.com>
2023-06-09 22:07:33 +1200

    New Code Version 0.0.2

--- app/src/main/java/com/example/capybaragamever001/MainThread.java
+++ app/src/main/java/com/example/capybaragamever001/MainThread.java
@@ -9,19 +9,20 @@ public class MainThread {
     public final static int MAX_FPS=28;
     //Average FPS=MAX FPS so the FPS during runtime is always 28
     private double averageFPS;
-    private surfaceHolder surfaceHolder;
+    private SurfaceHolder surfaceHolder;
     private GamePannel gamePannel;
     private Boolean running;
     public static Canvas canvas;
 
-    public MainThread(SurfaceHolderurfaceHolder surfaceHolder, GamePannel gamePannel){
+    public MainThread(SurfaceHolder surfaceHolder, GamePannel gamePannel){
         super();
         this.surfaceHolder = surfaceHolder;
         this.gamePannel = gamePannel;
 
     }
 
-    @Override
+        @Override
+
     public void run(){
 
         //Uses intergers
@@ -38,17 +39,49 @@ public class MainThread {
             canvas = null;
 
             try{
-                canvas = surfaceHolder.lockCanvas;
+
+
+                canvas = this.surfaceHolder.lockCanvas();
                 // MAIN Movement update system here
 
                 synchronized (surfaceHolder){
-
+                    //The "Meat of the entire game"
                 this.gamePannel.update();
                 this.gamePannel.draw(canvas);
 
                 }
             } catch (Exception e) {
-                throw new RuntimeException(e);
+                e.printStackTrace();
+            }finally {
+
+                if (canvas != null){
+                try {
+                    surfaceHolder.unlockCanvasAndPost(canvas);
+
+                } catch (Exeption e) {e.printStackTrace();}
+
+                }
+            }
+            //Milliseconds
+            timeMills = (System.nanoTime() = startTime/1000000;
+            waitTime = targetTime - timeMills;
+
+            try{
+                //Enforces the frame rate
+                if (waitTime>0)
+                    this.sleep(waitTime);
+
+        }catch (Exception e) {e.printStackTrace(); }
+
+            totalTime += System.nanoTime() - startTime;
+            frameCount++;
+            if (frameCount == MAX_FPS ){
+
+                //Brackets
+                averageFPS = 1000{(totalTime/frameCount)/1000000};
+
+            }
+
             }
 
 
@@ -65,4 +98,4 @@ public class MainThread {
 
 
 
-}
+
